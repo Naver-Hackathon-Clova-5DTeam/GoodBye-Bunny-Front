ef07b01ec78a3d513f241ee5a2309c0f3d96597b
LJH <049494@ewhain.net>
2023-12-13 22:49:59 +0900

    [Style] max-width 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/tailwind.css";
 import { BrowserRouter } from "react-router-dom";
+
+interface MaxWidthContainerProps {
+  children: ReactNode;
+}
+
+const MaxWidthContainer = ({ children }: MaxWidthContainerProps) => (
+  <div className="max-w-[480px] flex flex-col mr-auto ml-auto">{children}</div>
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <MaxWidthContainer>
+      <App />
+    </MaxWidthContainer>
   </BrowserRouter>
 );
--- src/pages/RecommendPage.tsx
+++ src/pages/RecommendPage.tsx
@@ -3,8 +3,8 @@ import NavBar from "../components/common/NavBar";
 
 const RecommendPage = () => {
   return (
-    <div className="flex items-center justify-center bg-black w-[100vw] h-[100vh]">
-      {/* <Button text="시작하기" isActive={true} /> */}
+    <div className="flex items-center justify-center bg-deepGray h-[100vh]">
+      <Button text="시작하기" isActive={true} />
       <NavBar />
     </div>
   );
