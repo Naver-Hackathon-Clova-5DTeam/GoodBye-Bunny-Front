ea3910708053cf6dd60e75903aa9c1f04de6097f
LJH <049494@ewhain.net>
2023-12-15 04:49:32 +0900

    [Style] 내가 작성한 회고 수정 기능

--- src/components/Post/PostBox.tsx
+++ src/components/Post/PostBox.tsx
@@ -1,6 +1,7 @@
 import { formatDate } from "../../utils/formatDate";
 import CommentBox from "./CommentBox";
 import editIcon from "../../assets/post/edit.png";
+import { useState, ChangeEvent, useEffect, useRef } from "react";
 
 const PostBox = ({
   profile,
@@ -10,9 +11,29 @@ const PostBox = ({
   updateTime,
   comments,
 }: CardType) => {
+  const [newContent, setNewContent] = useState(content);
+  const [isEdit, setIsEdit] = useState(false);
+  const [minHeight, setMinHeight] = useState("auto");
   const date = updateTime && formatDate(updateTime);
   const path = window.location.pathname;
   const isMine = path === "/mypost";
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      setMinHeight(`${textareaRef.current.scrollHeight}px`);
+    }
+  }, [isEdit]);
+
+  const editPost = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleTextareaInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    e.target.style.height = "auto";
+    e.target.style.height = `${e.target.scrollHeight}px`;
+    setNewContent(e.target.value);
+  };
 
   // 내가 작성한 회고는 로컬에서 프로필 가져오기
   return (
@@ -27,12 +48,24 @@ const PostBox = ({
           <img
             src={editIcon}
             className="w-[24px] ml-auto mr-1 cursor-pointer"
+            onClick={editPost}
           />
         )}
       </div>
       <div className="flex flex-col bg-white pl-4 pr-4 pb-9 pt-4">
         <h1 className="font-bold text-mdTitle mb-[14px]">{title}</h1>
-        <p className="font-regular text-fontGray">{content}</p>
+        {isEdit ? (
+          <textarea
+            autoFocus
+            ref={textareaRef}
+            value={newContent}
+            className="font-regular text-fontGray"
+            onChange={handleTextareaInput}
+            style={{ height: "auto", resize: "none", minHeight: minHeight }}
+          />
+        ) : (
+          <p className="font-regular text-fontGray">{content}</p>
+        )}
       </div>
       <div className="w-[100%] h-[1px] bg-lightGray"></div>
       {comments && <CommentBox comments={comments} />}
